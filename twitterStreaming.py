6c29e6c535bbccadf1f8fd38c29f003fdc720434
Pramud Bommakanti <pramud@outlook.com>
2016-11-16 11:32:22 +0530

    Update twitterStreaming.py

--- twitterStreaming.py
+++ twitterStreaming.py
@@ -1,3 +1,16 @@
+import tweepy
+from tweepy import OAuthHandler
+
+consumer_key = 'CONSUMER-KEY'
+consumer_secret = 'CONSUMER_SECRET'
+access_token = 'ACCESS-TOKEN'
+access_secret = 'ACCESS-SECRET'
+
+auth = OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_secret)
+
+api = tweepy.API(auth)
+
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 
