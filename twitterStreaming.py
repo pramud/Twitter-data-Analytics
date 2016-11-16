d2878d8f408a37864d7e5855184beae93e3b3f4a
Pramud Bommakanti <pramud@outlook.com>
2016-11-16 11:31:42 +0530

    Update twitterStreaming.py

--- twitterStreaming.py
+++ twitterStreaming.py
@@ -2,3 +2,17 @@ from tweepy import Stream
 from tweepy.streaming import StreamListener
 
 class MyListener(StreamListener):
+  def on_data(self,data):
+    try:
+      with open('python.json','a') as f:
+        f.write(data)
+        return True
+    except BaseException as e:
+      print("Error on data : %s" % str(e))
+    return True
+  def on_error(self, status):
+    print(status)
+    return True
+  
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=['#python'])
