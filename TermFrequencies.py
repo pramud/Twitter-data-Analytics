7617fbbbea6662112ec098008c30c6963f9ecf7a
Pramud Bommakanti <pramud@outlook.com>
2016-11-16 14:43:55 +0530

    Update TermFrequencies.py

--- TermFrequencies.py
+++ TermFrequencies.py
@@ -1,1 +1,43 @@
+import operator 
+import json
+from collections import Counter
+ 
+fname = 'mytweets.json'
+with open(fname, 'r') as f:
+    count_all = Counter()
+    for line in f:
+        tweet = json.loads(line)
+        # Create a list with all the terms
+        terms_all = [term for term in preprocess(tweet['text'])]
+        # Update the counter
+        count_all.update(terms_all)
+    # Print the first 5 most frequent words
+    print(count_all.most_common(5))
+    
+#Removing stopwords
 
+from nltk.corpus import stopwords
+import string
+ 
+punctuation = list(string.punctuation)
+stop = stopwords.words('english') + punctuation + ['rt', 'via']
+
+terms_stop = [term for term in preprocess(tweet['text']) if term not in stop]
+
+#More term filters
+
+# Count terms only once, equivalent to Document Frequency
+terms_single = set(terms_all)
+# Count hashtags only
+terms_hash = [term for term in preprocess(tweet['text']) 
+              if term.startswith('#')]
+# Count terms only (no hashtags, no mentions)
+terms_only = [term for term in preprocess(tweet['text']) 
+              if term not in stop and
+              not term.startswith(('#', '@'))] 
+              # mind the ((double brackets))
+              # startswith() takes a tuple (not a list) if 
+              # we pass a list of inputs
+      
+from nltk import bigrams  
+terms_bigram = bigrams(terms_stop)
