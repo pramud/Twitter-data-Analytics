8cf92cd79a18c4e9104658627c6a3fe9ffdc35e5
Pramud Bommakanti <pramud@outlook.com>
2016-11-16 11:38:05 +0530

    Update preprocessing_text.py

--- preprocessing_text.py
+++ preprocessing_text.py
@@ -1,1 +1,6 @@
 import json
+
+with open('mytweets.json','r') as f:
+  line = f.readline() # read only the first tweet/line
+  tweet = json.loads(line) # load it as Python dict
+  print(json.dumps(tweet, indent=4)) # pretty-print
